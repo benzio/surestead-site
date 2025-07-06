a4264c7ddc0b178beb01cebaee8e81aaab5c9ec0
benzio <jeff@atheema.com>
2025-07-05 22:52:13 -0700

    1st Round Updates Services

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
     './public/**/*.html',
   ],
   theme: {
+    screens: {
+      'xs': '475px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         outfit: ['Outfit', 'sans-serif'],
